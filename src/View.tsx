5f3d5e6c27734361aed0765fb3c3553b53f1b672
Shahid <afraz.shahid@gmail.com>
2022-08-27 00:49:11 +0500

    no debug

--- src/View.tsx
+++ src/View.tsx
@@ -265,9 +265,6 @@ export function View({
                 allowFileAccessFromFileURLs
                 allowFileAccess
                 onShouldStartLoadWithRequest={(request) => {
-                  console.log('request,',request)
-                  console.log('request.mainDocumentURL',request.mainDocumentURL);
-                  console.log('request.url',request.url)
                   if (
                     !isRendering &&
                     request.mainDocumentURL &&
